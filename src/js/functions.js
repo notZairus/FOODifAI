893a9fbb4e7a136156d3f5552ddef67bc40d2b03
notZairus <notzairus@gmail.com>
2025-01-21 08:43:06 +0700

    changed of image hosting server from imgbb to imghippo

--- src/js/functions.js
+++ src/js/functions.js
@@ -4,21 +4,23 @@
 async function urlToImg(imgUrl) {
   const response = await fetch(imgUrl);
   const blob = await response.blob();
-  return blob;
+
+  const imgFile = new File([blob], 'images.png', { type: 'image/png' })
+  return imgFile;
 }
 
 async function postToServer(imgBlob) {
   let fdata = new FormData();
-  fdata.append('image', imgBlob);
-
-  let key = import.meta.env.VITE_IMGBB_API_KEY;
+  fdata.append('file', imgBlob);
+  fdata.append('api_key', import.meta.env.VITE_IMGHIPPO_API_KEY);
   
-  let response = await fetch(`https://api.imgbb.com/1/upload?key=${key}`, {
+  let response = await fetch(`https://api.imghippo.com/v1/upload`, {
     method: "POST",
     body: fdata
   });
 
   let result = await response.json();
+
   return result.data.url;
 }
 
